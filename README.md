4b5cb3217f20096a67e767e33c06e2cefe38e5f3
Debian <admin@ip-15-0-59-110.us-east-2.compute.internal>
2020-05-30 14:22:35 +0000

    EBPF, java profiling and flamegraph

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # java-profiling
 Profiling Java applications using eBPF
+
+Links:
+
+https://wenfeng-gao.github.io/post/profile-java-program-with-bcc-tool/
+https://github.com/brendangregg/FlameGraph
+https://github.com/jvm-profiling-tools/perf-map-agent
+
+
+https://www.acticloud.eu/storage/app/uploads/public/5ce/d1a/e36/5ced1ae36ceba475001781.pdf
+
+http://www.brendangregg.com/blog/2015-05-15/ebpf-one-small-step.html
+
+
---
+++ java-profile.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -ex
+
+#PID=15048
+#CTN_ID=ab3ef93ec56d
+PID=15048
+CTN_ID=b2e03cc3a967
+JAVA_HOME=`docker exec ${CTN_ID} java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' |cut -d'=' -f2|sed -e 's/\/jre//' | xargs`
+JAVA_BIN=`docker exec ${CTN_ID} ps -ef | grep jav[a] | awk '{print $4}'`
+#JAVA_BIN=$JAVA_HOME/bin/java
+JAVA_PID=`docker exec ${CTN_ID} ps -ef | grep ${JAVA_BIN} | awk '{ print $1 }'`
+JAVA_UID=`docker exec ${CTN_ID} ps -e -o pid,user,group,comm | grep ${JAVA_BIN} | awk '{ print $2 }'`
+JAVA_GID=`docker exec ${CTN_ID} ps -e -o pid,user,group,comm | grep ${JAVA_BIN} | awk '{ print $3 }'`
+
+echo ">>>>Vinuth>>>"
+echo $JAVA_HOME
+echo $JAVA_BIN
+echo $JAVA_PID
+echo $JAVA_UID
+echo $JAVA_GID
+# We copy libperfmap.so and attach-main.jar to target container
+docker cp /home/admin/Vinuth/perf-map-agent-master/out ${CTN_ID}:/tmp/perf-map-agent
+
+# Command to populate a /tmp/perf-PID.map file with the symbols
+docker exec --user "${JAVA_UID}":"${JAVA_GID}" "${CTN_ID}" sh -c "cd /tmp/perf-map-agent && java -XX:+StartAttachListener -cp /tmp/perf-map-agent/attach-main.jar:${JAVA_HOME}/lib/tools.jar net.virtualvoid.perf.AttachOnce ${JAVA_PID}"
+
+# Copy back to our perf container
+docker cp ${CTN_ID}:/tmp/perf-${JAVA_PID}.map /tmp/perf-${PID}.map
+
+# We profile 15 seconds
+/usr/share/bcc/tools/profile -adf -p $PID 30 > $PID.profile
+
+# Draw flamegraph
+/root/FlameGraph/flamegraph.pl < $PID.profile --colors java --hash > $PID.svg
